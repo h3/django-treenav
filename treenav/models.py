a5c5aff3c469f2eb958aeb41211d6953aa2cc068
h3 <haineault@gmail.com>
2011-09-09 14:27:37 -0300

    Edited treenav/models.py via GitHub

--- treenav/models.py
+++ treenav/models.py
@@ -146,7 +146,7 @@ class MenuItem(models.Model):
     objects = MenuItemManager()
     
     class Meta:
-        ordering = ('order', 'lft', 'tree_id')
+        ordering = ('lft', 'tree_id')
     
     def to_tree(self):
         cache_key = 'menu-tree-%s' % self.slug
